db5b71ce182a0feeca9dbc7c159b12caf29f80b6
suny <sun.yang@dlt-world.com>
2019-05-22 10:44:06 +0800

    suny20190522

--- src/page/public/layout.vue
+++ src/page/public/layout.vue
@@ -1,17 +1,41 @@
 <template>
     <div id="layout">
-        <syHeader @CB-Header="CB_Header"></syHeader>
+        <syHeader @CB-Header="CB_Header" id="sy-header"></syHeader>
         <transition :style="{'min-height':cliHeight+'px'}">
-            <router-view :key="$route.name" />
+            <router-view :key="$route.name" id="layouts" />
         </transition>
     </div>
 </template>
 
 <script>
+import $ from "jquery";
 import syHeader from "@/page/public/header";
+var windowTop = 667;
+window.addEventListener("touchmove", function(e) {
+    if (e.targetTouches[0].pageY < windowTop) {
+        if (e.targetTouches[0].pageY > 60) {
+            $("#sy-header").css("display", "none");
+        }
+        windowTop = e.targetTouches[0].pageY;
+    } else if (e.targetTouches[0].pageY > windowTop) {
+        if ($("#sy-header").css("display") == "block") {
+            windowTop = e.targetTouches[0].pageY;
+            return;
+        }
+        $("#sy-header").css("display", "block");
+        windowTop = e.targetTouches[0].pageY;
+    }
+});
 export default {
     name: "layout",
     components: { syHeader },
+    watch: {
+        $route(newValue) {
+            if (newValue) {
+                $("#sy-header").css("display", "block");
+            }
+        }
+    },
     methods: {
         CB_Header(val) {
             if (val) {
